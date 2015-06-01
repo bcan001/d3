ffaa1a658c24d31995b5cde42edfe660ca60d795
Ben Caneba <bcaneba@gmail.com>
2015-06-01 17:44:16 -0500

    review

---
+++ random2.rb
@@ -0,0 +1,21 @@
+# algorithm practice
+
+def minCalc(array)
+	# comparisons
+	array.length - 1
+end
+
+
+def maxMinCalc(array)
+	# comparisons
+	# 2 * (array.length / 2) + 2
+
+	# 4 * (array.length / 4) + 6
+
+	(3 * array.length) / 2 - 2
+
+end
+
+
+puts minCalc([1,2,3,4])
+puts maxMinCalc([1,2,3,4])
